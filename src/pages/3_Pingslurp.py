4d5bca5e2551749ca4b48b5a367e225930455e3d
Brian of London <brian@podping.org>
2022-11-05 20:01:41 +0200

    add all_iris
    Signed-off-by: Brian of London <brian@podping.org>

--- src/pages/3_Pingslurp.py
+++ src/pages/3_Pingslurp.py
@@ -6,47 +6,181 @@ import streamlit as st
 from plotly.subplots import make_subplots
 from pymongo import MongoClient
 
+
 # DB_CONNECTION="mongodb://100.68.99.92:27017/"
 DB_CONNECTION = "mongodb://cepo-v4vapp:27017/"
+CLIENT = MongoClient(DB_CONNECTION)
 
-client = MongoClient(DB_CONNECTION)
+def all_trans_by_account():
+    result = CLIENT["pingslurp"]["meta_ts"].aggregate(
+        [
+            {"$match": {"metadata.posting_auth": {"$ne": "podping.test"}}},
+            {
+                "$group": {
+                    "_id": {
+                        "account": "$metadata.posting_auth",
+                        "timestamp": {
+                            "$dateTrunc": {
+                                "date": "$timestamp",
+                                "unit": time_frame,
+                                "timezone": "Asia/Jerusalem",
+                            }
+                        },
+                    },
+                    "timestamp": {"$first": "$timestamp"},
+                    "account": {"$first": "$metadata.posting_auth"},
+                    "avg_size": {"$avg": "$json_size"},
+                    "total_size": {"$sum": "$json_size"},
+                    "total_podpings": {"$sum": 1},
+                    "total_iris": {"$sum": "$num_iris"},
+                }
+            },
+            {"$sort": {"_id.timestamp": -1}},
+        ]
+    )
+    return result
+
+def all_trans_no_account():
+    result_no_account = CLIENT['pingslurp']['meta_ts'].aggregate([
+        {
+            '$match':
+                {
+                'metadata.posting_auth': {'$ne': 'podping.test'}
+                }
+        },{
+            '$group': {
+                '_id': {
+                    'timestamp': {
+                        '$dateTrunc': {
+                            'date': '$timestamp',
+                            'unit': time_frame,
+                            'timezone': 'Asia/Jerusalem'
+                        }
+                    }
+                },
+                'timestamp' :{
+                    '$first': '$timestamp'
+                },
+                'avg_size': {
+                    '$avg': '$json_size'
+                },
+                'total_size': {
+                    '$sum': '$json_size'
+                },
+                'total_podpings': {
+                    '$sum': 1
+                },
+                'total_iris': {
+                    '$sum': '$num_iris'
+                },
+            }
+        }, {
+            '$sort': {
+                '_id.timestamp': -1
+            }
+        }
+    ])
+    return result_no_account
+
+def hour_trans_by_account(time_limit: datetime, time_frame: str):
+    result = CLIENT['pingslurp']['meta_ts'].aggregate([
+        {
+            '$match':
+                {
+                'timestamp': {'$gt': time_limit},
+                }
+        },{
+            '$group': {
+                '_id': {
+                    'account': '$metadata.posting_auth',
+                    'timestamp': {
+                        '$dateTrunc': {
+                            'date': '$timestamp',
+                            'unit': time_frame,
+                            'timezone': 'Asia/Jerusalem'
+                        }
+                    }
+                },
+                'timestamp' :{
+                    '$first': '$timestamp' },
+                'account': {
+                    '$first': '$metadata.posting_auth'
+                },
+                'avg_size': {
+                    '$avg': '$json_size'
+                },
+                'total_size': {
+                    '$sum': '$json_size'
+                },
+                'total_podpings': {
+                    '$sum': 1
+                },
+                'total_iris': {
+                    '$sum': '$num_iris'
+                }
+            }
+        }, {
+            '$sort': {
+                '_id.timestamp': -1
+            }
+        }
+    ])
+    return result
+
+def hour_trans_no_account(time_limit: datetime, time_frame: str):
+    result = CLIENT['pingslurp']['meta_ts'].aggregate([
+        {
+            '$match':
+                {
+                'timestamp': {'$gt': time_limit},
+                }
+        },{
+            '$group': {
+                '_id': {
+                    'timestamp': {
+                        '$dateTrunc': {
+                            'date': '$timestamp',
+                            'unit': time_frame,
+                            'timezone': 'Asia/Jerusalem'
+                        }
+                    }
+                },
+                'timestamp' :{
+                    '$first': '$timestamp' },
+                'avg_size': {
+                    '$avg': '$json_size'
+                },
+                'total_size': {
+                    '$sum': '$json_size'
+                },
+                'total_podpings': {
+                    '$sum': 1
+                },
+                'total_iris': {
+                    '$sum': '$num_iris'
+                }
+            }
+        }, {
+            '$sort': {
+                '_id.timestamp': -1
+            }
+        }
+    ])
+    return result
+
+
 st.set_page_config(layout="wide")
 time_frame = "hour"
-result = client["pingslurp"]["meta_ts"].aggregate(
-    [
-        {"$match": {"metadata.posting_auth": {"$ne": "podping.test"}}},
-        {
-            "$group": {
-                "_id": {
-                    "account": "$metadata.posting_auth",
-                    "timestamp": {
-                        "$dateTrunc": {
-                            "date": "$timestamp",
-                            "unit": time_frame,
-                            "timezone": "Asia/Jerusalem",
-                        }
-                    },
-                },
-                "timestamp": {"$first": "$timestamp"},
-                "account": {"$first": "$metadata.posting_auth"},
-                "avg_size": {"$avg": "$json_size"},
-                "total_size": {"$sum": "$json_size"},
-                "total_podpings": {"$sum": 1},
-                "total_iris": {"$sum": "$num_iris"},
-            }
-        },
-        {"$sort": {"_id.timestamp": -1}},
-    ]
-)
-
+result = all_trans_by_account()
 df = pd.DataFrame(result)
 df.set_index("timestamp", inplace=True)
+
+df_no_account = pd.DataFrame(all_trans_no_account())
+df_no_account.set_index('timestamp',inplace=True)
+
 all_accounts = df.account.unique()
 all_accounts.sort()
 fig = make_subplots(specs=[[{"secondary_y": True}]])
-# fig.add_trace(
-#     go.Scatter(x=df_no_account.index, y=df_no_account.total_iris, name="All Iris")
-# )
 for account in all_accounts:
     fig.add_trace(
         go.Scatter(
@@ -57,6 +191,9 @@ for account in all_accounts:
         ),
         secondary_y=True,
     )
+fig.add_trace(
+    go.Scatter(x=df_no_account.index, y=df_no_account.total_iris, name="All Iris")
+)
 
 fig.update_layout(
     legend=dict(orientation="h", yanchor="bottom", y=-0.25, xanchor="right", x=0.9)
@@ -71,51 +208,13 @@ number_hours = 4
 
 time_frame = 'minute'
 time_limit =  datetime.utcnow() - timedelta(hours=number_hours)
-result = client['pingslurp']['meta_ts'].aggregate([
-    {
-        '$match':
-            {
-            'timestamp': {'$gt': time_limit},
-            }
-    },{
-        '$group': {
-            '_id': {
-                'account': '$metadata.posting_auth',
-                'timestamp': {
-                    '$dateTrunc': {
-                        'date': '$timestamp',
-                        'unit': time_frame,
-                        'timezone': 'Asia/Jerusalem'
-                    }
-                }
-            },
-            'timestamp' :{
-                '$first': '$timestamp' },
-            'account': {
-                '$first': '$metadata.posting_auth'
-            },
-            'avg_size': {
-                '$avg': '$json_size'
-            },
-            'total_size': {
-                '$sum': '$json_size'
-            },
-            'total_podpings': {
-                '$sum': 1
-            },
-            'total_iris': {
-                '$sum': '$num_iris'
-            }
-        }
-    }, {
-        '$sort': {
-            '_id.timestamp': -1
-        }
-    }
-])
-
+result = hour_trans_by_account(time_limit=time_limit, time_frame=time_frame)
 df_hour = pd.DataFrame(result)
 df_hour.set_index("timestamp", inplace=True)
+
+df_hour_no_account = pd.DataFrame(hour_trans_no_account(time_limit,time_frame))
+df_hour_no_account.set_index("timestamp", inplace=True)
+
 all_accounts = df_hour.account.unique()
 all_accounts.sort()
 fig2 = make_subplots(specs=[[{"secondary_y": True}]])
@@ -131,6 +230,9 @@ for account in all_accounts:
 
         secondary_y=True,
     )
+fig2.add_trace(
+    go.Scatter(x=df_hour_no_account.index, y=df_hour_no_account.total_iris, name="All Iris")
+)
 
 fig2.update_layout(
     legend=dict(orientation="h", yanchor="bottom", y=-0.25, xanchor="right", x=0.9)
