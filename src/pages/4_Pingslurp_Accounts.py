1b62df54ba85f091e4cae0733be5b1c11e300857
Brian of London <brian@podping.org>
2022-11-24 12:19:55 +0200

    added livetest filter
    Signed-off-by: Brian of London <brian@podping.org>

--- src/pages/4_Pingslurp_Accounts.py
+++ src/pages/4_Pingslurp_Accounts.py
@@ -1,4 +1,5 @@
 import os
+import re
 from datetime import datetime, timedelta
 
 import pandas as pd
@@ -15,10 +16,16 @@ DB_CONNECTION = os.getenv("DB_CONNECTION")
 CLIENT = MongoClient(DB_CONNECTION)
 
 
+exclude_livetest = {"$match": {"metadata.id": re.compile(r"pp_.*")}}
+livetest_only = {"$match": {"metadata.id": re.compile(r"pply_.*")}}
+include_livetest = {}
+livetest_filter = livetest_only
+
+
 def all_trans_by_account():
     result = CLIENT["pingslurp"]["meta_ts"].aggregate(
         [
-            # {"$match": {"metadata.posting_auth": {"$ne": "podping.test"}}},
+            st.session_state.livetest_filter,
             {
                 "$group": {
                     "_id": {
@@ -48,7 +55,7 @@ def all_trans_by_account():
 def all_trans_no_account():
     result_no_account = CLIENT["pingslurp"]["meta_ts"].aggregate(
         [
-            # {"$match": {"metadata.posting_auth": {"$ne": "podping.test"}}},
+            st.session_state.livetest_filter,
             {
                 "$group": {
                     "_id": {
@@ -81,6 +88,7 @@ def hour_trans_by_account(time_limit: datetime, time_frame: str):
                     "timestamp": {"$gt": time_limit},
                 }
             },
+            st.session_state.livetest_filter,
             {
                 "$group": {
                     "_id": {
@@ -115,6 +123,7 @@ def hour_trans_no_account(time_limit: datetime, time_frame: str):
                     "timestamp": {"$gt": time_limit},
                 }
             },
+            st.session_state.livetest_filter,
             {
                 "$group": {
                     "_id": {
@@ -147,12 +156,32 @@ metrics = {
 }
 
 
+livetest_filter = {
+    "Exclude Live Tests": {"$match": {"metadata.id": re.compile(r"pp_.*")}},
+    "Include Live Tests": {"$match": {}},
+    "Only Live Tests": {"$match": {"metadata.id": re.compile(r"pplt_.*")}},
+}
+
+
 st.set_page_config(layout="wide")
 st.session_state.metric = st.sidebar.selectbox(
     label="Metric", options=metrics.keys(), help="Metric to show"
 )
+st.session_state.livetest_choice = st.sidebar.selectbox(
+    label="Live Tests",
+    options=livetest_filter.keys(),
+    help=(
+        "Show/Hide Live Tests. "
+        "Some fake podpings are sent out during testing, "
+        "these are usually hidden but can be shown with this option."
+    ),
+)
+
+st.session_state.livetest_filter = livetest_filter[st.session_state.livetest_choice]
+
 st.sidebar.markdown(ALL_MARKDOWN["pingslurp_accounts"])
 choice = st.session_state.metric
+
 
 metric = metrics[choice]
 metric_desc = choice
@@ -161,65 +190,69 @@ time_frame = "hour"
 
 result = all_trans_by_account()
 df = pd.DataFrame(result)
-df.set_index("timestamp", inplace=True)
-df["total_size_mb"] = df["total_size"] / (1024 * 1024)
+st.title(body=f"{metric_desc} Sent per Hour by each account")
+if df.empty:
+    st.markdown(f"## No data")
 
-df_no_account = pd.DataFrame(all_trans_no_account())
-df_no_account.set_index("timestamp", inplace=True)
-df_no_account["total_size_mb"] = df_no_account["total_size"] / (1024 * 1024)
+else:
+    df.set_index("timestamp", inplace=True)
+    df["total_size_mb"] = df["total_size"] / (1024 * 1024)
 
-all_accounts = df.account.unique()
-all_accounts.sort()
-fig = make_subplots(specs=[[{"secondary_y": True}]])
-for account in all_accounts:
+    df_no_account = pd.DataFrame(all_trans_no_account())
+    df_no_account.set_index("timestamp", inplace=True)
+    df_no_account["total_size_mb"] = df_no_account["total_size"] / (1024 * 1024)
+
+    all_accounts = df.account.unique()
+    all_accounts.sort()
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    for account in all_accounts:
+        fig.add_trace(
+            go.Scatter(
+                x=df[df.account == account].index,
+                y=df[df.account == account][metric],
+                name=account,
+                mode="markers",
+                marker=dict(size=5 + df[df.account == account].total_size / 2 ** 14),
+            ),
+            secondary_y=True,
+        )
+    fig.add_trace(
+        go.Scatter(x=df_no_account.index, y=df_no_account[metric], name=metric_desc)
+    )
+
     fig.add_trace(
         go.Scatter(
-            x=df[df.account == account].index,
-            y=df[df.account == account][metric],
-            name=account,
-            mode="markers",
-            marker=dict(size=5 + df[df.account == account].total_size / 2 ** 14),
-        ),
-        secondary_y=True,
+            x=df_no_account.index,
+            y=df_no_account[metric].rolling(24).mean(),
+            name=f"{metric_desc} (24H avg)",
+        )
     )
-fig.add_trace(
-    go.Scatter(x=df_no_account.index, y=df_no_account[metric], name=metric_desc)
-)
 
-fig.add_trace(
-    go.Scatter(
-        x=df_no_account.index,
-        y=df_no_account[metric].rolling(24).mean(),
-        name=f"{metric_desc} (24H avg)",
+    # fig.update_layout(
+    #     legend=dict(orientation="h", yanchor="bottom", y=-0.25, xanchor="right", x=0.9)
+    # )
+    # fig.update_layout(legend=dict(yanchor="top", y=0.1))
+    fig.update_layout(margin={"autoexpand": True, "b": 25, "t": 25, "l": 5, "r": 5})
+    fig.update_yaxes(title_text=f"{metric_desc}", secondary_y=False, showgrid=False)
+    fig.update_yaxes(
+        title_text=f"{metric_desc} by account", secondary_y=True, showgrid=False
     )
-)
 
-# fig.update_layout(
-#     legend=dict(orientation="h", yanchor="bottom", y=-0.25, xanchor="right", x=0.9)
-# )
-fig.update_layout(margin={"autoexpand": True, "b": 20, "t": 0, "l": 0, "r": 0})
-fig.update_yaxes(title_text=f"{metric_desc}", secondary_y=False, showgrid=False)
-fig.update_yaxes(
-    title_text=f"{metric_desc} by account", secondary_y=True, showgrid=False
-)
+    fig.update_xaxes(showspikes=True)
+    fig.update_yaxes(showspikes=True)
+    # Grid lines
+    fig.update_xaxes(gridwidth=0.1, gridcolor="LightGrey")
+    fig.update_yaxes(showline=False, gridwidth=0.1)
+    fig.update_yaxes(gridwidth=0.1, secondary_y=True)
 
-fig.update_xaxes(showspikes=True)
-fig.update_yaxes(showspikes=True)
-# Grid lines
-# fig.update_xaxes(gridwidth=0.1, gridcolor="LightGrey")
-# fig.update_yaxes(showline=False, gridwidth=0.1)
-# fig.update_yaxes(gridwidth=0.1, secondary_y=True)
+    fig.update_layout(title_x=0.05, title_y=0.95)
+    fig.update_layout(title_text=f"{metric_desc} Sent per Hour by each account")
 
-fig.update_layout(title_x=0.05, title_y=0.95)
-fig.update_layout(title_text=f"{metric_desc} Sent per Hour by each account")
+    end_range = datetime.utcnow() + timedelta(hours=0.5)
+    start_range = end_range - timedelta(days=30)
+    fig.update_layout(xaxis=dict(range=[start_range, end_range]))
 
-end_range = datetime.utcnow() + timedelta(hours=0.5)
-start_range = end_range - timedelta(days=30)
-fig.update_layout(xaxis=dict(range=[start_range, end_range]))
-
-
-st.title(body=f"{metric_desc} Sent per Hour by each account")
-st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
 
 number_hours = 4
 
@@ -227,59 +260,67 @@ time_frame = "minute"
 time_limit = datetime.utcnow() - timedelta(hours=number_hours)
 result = hour_trans_by_account(time_limit=time_limit, time_frame=time_frame)
 df_hour = pd.DataFrame(result)
-df_hour.set_index("timestamp", inplace=True)
-df_hour["total_size_mb"] = df_hour["total_size"] / (1024 * 1024)
+st.title(f"Last {number_hours} hours of {metric_desc} per minute by Accounts")
+if df_hour.empty:
+    st.markdown("## No data")
 
-df_hour_no_account = pd.DataFrame(hour_trans_no_account(time_limit, time_frame))
-df_hour_no_account.set_index("timestamp", inplace=True)
-df_hour_no_account["total_size_mb"] = df_hour_no_account["total_size"] / (1024 * 1024)
+else:
+    df_hour.set_index("timestamp", inplace=True)
+    df_hour["total_size_mb"] = df_hour["total_size"] / (1024 * 1024)
 
-all_accounts = df_hour.account.unique()
-all_accounts.sort()
-fig2 = make_subplots(specs=[[{"secondary_y": True}]])
-for account in all_accounts:
+    df_hour_no_account = pd.DataFrame(hour_trans_no_account(time_limit, time_frame))
+    df_hour_no_account.set_index("timestamp", inplace=True)
+    df_hour_no_account["total_size_mb"] = df_hour_no_account["total_size"] / (
+        1024 * 1024
+    )
+
+    all_accounts = df_hour.account.unique()
+    all_accounts.sort()
+    fig2 = make_subplots(specs=[[{"secondary_y": True}]])
+    for account in all_accounts:
+        fig2.add_trace(
+            go.Scatter(
+                x=df_hour[df_hour.account == account].index,
+                y=df_hour[df_hour.account == account][metric],
+                name=account,
+                mode="markers",
+                marker=dict(
+                    size=10 + df_hour[df_hour.account == account].total_size / 256
+                ),
+            ),
+            secondary_y=True,
+        )
     fig2.add_trace(
         go.Scatter(
-            x=df_hour[df_hour.account == account].index,
-            y=df_hour[df_hour.account == account][metric],
-            name=account,
-            mode="markers",
-            marker=dict(size=10 + df_hour[df_hour.account == account].total_size / 256),
-        ),
-        secondary_y=True,
+            x=df_hour_no_account.index, y=df_hour_no_account[metric], name=metric_desc
+        )
     )
-fig2.add_trace(
-    go.Scatter(
-        x=df_hour_no_account.index, y=df_hour_no_account[metric], name=metric_desc
+    fig2.add_trace(
+        go.Scatter(
+            x=df_hour_no_account.index,
+            y=df_hour_no_account[metric].rolling("1H").mean(),
+            name=f"{metric_desc} (1hr Avg)",
+        )
     )
-)
-fig2.add_trace(
-    go.Scatter(
-        x=df_hour_no_account.index,
-        y=df_hour_no_account[metric].rolling('1H').mean(),
-        name=f"{metric_desc} (1hr Avg)",
+
+    # fig2.update_layout(
+    #     legend=dict(orientation="v", yanchor="top", y=0, xanchor="auto", x=1.15)
+    # )
+    fig2.update_layout(margin={"autoexpand": True, "b": 25, "t": 25, "l": 5, "r": 5})
+
+    fig2.update_yaxes(title_text=metric_desc, secondary_y=False, showgrid=False)
+    fig2.update_layout(title_x=0.05, title_y=0.95)
+
+    fig2.update_yaxes(
+        title_text=f"{metric_desc} by account", secondary_y=True, showgrid=False
     )
-)
+    fig2.update_layout(
+        title_text=f"Last {number_hours} hours of {metric_desc} per minute by Accounts"
+    )
 
+    fig2.update_xaxes(showspikes=True)
+    fig2.update_yaxes(showspikes=True)
 
-# fig2.update_layout(
-#     legend=dict(orientation="v", yanchor="top", y=0, xanchor="auto", x=1.15)
-# )
-fig2.update_layout(margin={"autoexpand": True, "b": 0, "t": 0, "l": 0, "r": 0})
+    st.plotly_chart(fig2, use_container_width=True)
 
-fig2.update_yaxes(title_text=metric_desc, secondary_y=False, showgrid=False)
-fig2.update_layout(title_x=0.05, title_y=0.95)
-
-fig2.update_yaxes(
-    title_text=f"{metric_desc} by account", secondary_y=True, showgrid=False
-)
-fig2.update_layout(
-    title_text=f"Last {number_hours} hours of {metric_desc} per minute by Accounts"
-)
-
-fig2.update_xaxes(showspikes=True)
-fig2.update_yaxes(showspikes=True)
-st.title(f"Last {number_hours} hours of {metric_desc} per minute by Accounts")
-st.plotly_chart(fig2, use_container_width=True)
-
-st.dataframe(df_hour.groupby("account")[metric].describe())
+    st.dataframe(df_hour.groupby("account")[metric].describe())
