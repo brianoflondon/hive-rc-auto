0d4d804a84902f0ed3b436dbfd9ca7ec4bd01a19
Brian of London (Dot) <brian@v4v.app>
2023-03-06 12:34:42 +0200

    added time range slider
    Signed-off-by: Brian of London (Dot) <brian@v4v.app>

--- src/pages/6_Duplicate_Feeds.py
+++ src/pages/6_Duplicate_Feeds.py
@@ -20,56 +20,54 @@ st.set_page_config(
     menu_items=None,
 )
 
+time_range = st.sidebar.slider(
+    "Time Range (hours) to examine:", min_value=-12, max_value=0, value=(-1, 0)
+)
+st.sidebar.write("Time Range", time_range)
+(start_hours, end_hours) = time_range
+num_hours = end_hours - start_hours
 
-duplicate_count = st.sidebar.slider(label="Duplicate Threshold", min_value=3, max_value=20, value=5)
 
-start_date = datetime.now(timezone.utc) - timedelta(hours=1)
-end_data = datetime.now(timezone.utc) - timedelta(hours=0)
+duplicate_count = st.sidebar.slider(
+    label="Duplicate Threshold", min_value=3, max_value=18 + num_hours * 2, value=5
+)
 
-result = CLIENT['pingslurp']['all_podpings'].aggregate(
+
+start_date = datetime.now(timezone.utc) + timedelta(hours=start_hours)
+end_data = datetime.now(timezone.utc) + timedelta(hours=end_hours)
+
+result = CLIENT["pingslurp"]["all_podpings"].aggregate(
     [
         {
-            '$match': {
+            "$match": {
                 "$and": [
                     {"timestamp": {"$gt": start_date}},
                     {"timestamp": {"$lt": end_data}},
                 ]
             }
-        }, {
-            '$unwind': {
-                'path': '$iris',
-                'includeArrayIndex': 'iri_pos',
-                'preserveNullAndEmptyArrays': True
+        },
+        {
+            "$unwind": {
+                "path": "$iris",
+                "includeArrayIndex": "iri_pos",
+                "preserveNullAndEmptyArrays": True,
             }
-        }, {
-            '$group': {
-                '_id': '$iris',
-                'count': {
-                    '$sum': 1
-                }
-            }
-        }, {
-            '$match': {
-                'count': {
-                    '$gte': duplicate_count
-                }
-            }
-        }, {
-            '$sort': {
-                'count': -1
-            }
-        }
+        },
+        {"$group": {"_id": "$iris", "count": {"$sum": 1}}},
+        {"$match": {"count": {"$gte": duplicate_count}}},
+        {"$sort": {"count": -1}},
     ]
 )
-st.sidebar.markdown(ALL_MARKDOWN['duplicated_feeds'])
-st.header(f"List of feeds with more than {duplicate_count} podpings in the last hour")
-
+st.sidebar.markdown(ALL_MARKDOWN["duplicated_feeds"])
+st.header(
+    f"List of feeds with more than {duplicate_count} podpings in {num_hours} hours."
+)
 
 
 df = pd.DataFrame(result)
-df.rename(columns={"_id":"iri"}, inplace=True)
+df.rename(columns={"_id": "iri"}, inplace=True)
 print(df)
 st.dataframe(df)
 
 for index, row in df.iterrows():
-    print(row['iri'], row['count'])
+    print(row["iri"], row["count"])
